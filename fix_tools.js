2c393bb2a080203c895026347e8664f2a972074a
Artem <psycho051378@gmail.com>
2026-06-26 23:27:01 +0300

    Delete fix_tools.js
    Remove unnecessary file

--- fix_tools.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let f = 'C:/Users/Arthomas/.openclaw/workspace/projects/Pai_Android1/app/src/main/java/com/pai/android/agent/DecisionEngine.kt';
-let c = fs.readFileSync(f, 'utf8');
-c = c.replace(
-  'com.pai.android.data.repository.buildNativeToolDefs(skillRegistry)',
-  'buildNativeToolDefs(setOf("web_search", "contacts_search", "sms_send", "weather_forecast", "file_system_search", "web_fetch"))'
-);
-fs.writeFileSync(f, c, 'utf8');
-console.log('OK');
